25143a24de5b8fd813561b0f66bc5706801616e3
dansmith09 <dansmith_09@hotmail.com>
2022-06-21 11:51:14 +1000

    Started fleshing out website skeleton

---
+++ logic.js
@@ -0,0 +1,6 @@
+// Define the HTML elements we will use in JS
+var APIKey = 'cf4280e902da44b2517b6186d055cd7c';
+// Get data from API
+// Make html content based on API data
+// Save previous searches to storage
+
