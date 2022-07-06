749fc31381b4c28b78f4d063e2062a3d9640b1a2
dansmith09 <dansmith_09@hotmail.com>
2022-07-06 13:50:17 +1000

    Got API calls working and converted kelvin to celcius

--- logic.js
+++ logic.js
@@ -6,18 +6,60 @@ var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${A
 // Make html content based on API data
 // Save previous searches to storage
 
-fetch(apiURL).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-          console.log(data.weather[0].icon);
-            var iconCode = data.weather[0].icon;
-            var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-            var newPic = document.createElement('img');
-            newPic.setAttribute('src',iconURL);
-            document.querySelector('header').appendChild(newPic);
-        });
-    }
-});
+var getCityForecast = function(city) {
+    // Changes URL to insert city in query parameter
+    apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    // Calls API
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                // Logs API call to console
+                console.log(data);
+                // gets icon code for current weather
+                var iconCode = data.weather[0].icon;
+                // concatenates code into url
+                var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+                // Gets weather icon
+                var weatherIcon = document.getElementById('weatherIcon');
+                // Sets icons source to iconURL to render icon
+                weatherIcon.setAttribute('src',iconURL);
+                // Gets city name and changes heading to the city name
+                document.getElementById('cityName').innerHTML = city;
+                var today = moment().format('MM-DD-YY');
+                document.getElementById('dateToday').innerHTML = "(" + today + ")";
+                document.getElementById('tempToday').innerHTML = "Temp: " + (data.main.temp - 273.15).toFixed(2) + ' °C';
+                document.getElementById('windToday').innerHTML = "Wind: " + data.wind.speed;
+                document.getElementById('humidityToday').innerHTML = "Humidity: " + data.main.humidity;
+                document.getElementById('UVIndexToday').innerHTML = "UV Index: " + city;
+                // Calls futureWeather function with long and lat as parameters
+                var lon = data.coord.lon;
+                var lat = data.coord.lat;
+                oneCall(lat, lon);
+                // Logs API URL
+                console.log(`api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=5&appid=${APIKey}`);
 
-// How do we get the future days from API call
+            });
+        } else {
+            console.log("Not a good response there must be an error eeeek!");
+        }
+    });
+}
+
+const oneCall = (lat, lon) => {
+    var newAPIURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${APIKey}`;
+
+    fetch(newAPIURL).then(function (response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                // Do all the stuff here
+                console.log(data);
+                console.log(data.daily[0].wind_speed);
+                // First Block
+
+            })
+        }
+    })
+}
+
+
+// Search Button Function 
