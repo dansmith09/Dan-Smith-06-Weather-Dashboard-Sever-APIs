cc5dfe7e4f998e208965e5980fb371a3326cbba6
dansmith09 <dansmith_09@hotmail.com>
2022-07-07 00:08:29 +1000

    Refactored JS and organised files

--- Assets/JS/logic.js
+++ Assets/JS/logic.js
@@ -1,6 +1,6 @@
 // Define the HTML elements we will use in JS
-var APIKey = 'cf4280e902da44b2517b6186d055cd7c';
-
+let APIKey = 'cf4280e902da44b2517b6186d055cd7c';
+// API call to get current weather info and lon and lat to call future forcast function
 const getCityForecast = (city) => {
     // Changes URL to insert city in query parameter
     apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
@@ -24,38 +24,40 @@ const getCityForecast = (city) => {
                 let weatherIcon = document.getElementById('weatherIcon');
                 // Sets icons source to iconURL to render icon
                 weatherIcon.setAttribute('src',iconURL);
-                // Gets city name and changes heading to the city name
+                // Renders all current weather info to todayResults section
                 document.getElementById('cityName').innerHTML = city;
-                var today = moment().format('MM-DD-YY');
+                let today = moment().format('MM-DD-YY');
                 document.getElementById('dateToday').innerHTML = "(" + today + ")";
                 document.getElementById('tempToday').innerHTML = "Temp: " + (data.main.temp - 273.15).toFixed(2) + ' °C';
                 document.getElementById('windToday').innerHTML = "Wind: " + data.wind.speed + " M/Hr";
                 document.getElementById('humidityToday').innerHTML = "Humidity: " + data.main.humidity;
                 // Calls futureWeather function with long and lat as parameters
-                var lon = data.coord.lon;
-                var lat = data.coord.lat;
+                let lon = data.coord.lon;
+                let lat = data.coord.lat;
                 oneCall(lat, lon);
             });
         } else {
-            alert('Invalid city name. Please check your spelling and try again.')
+            alert('Invalid city name.\nPlease check your spelling and try again.')
             console.log("Not a good response there must be an error eeeek!");
         }
     });
-}
-
+};
+// API call to get 5 day forecast and UV Index
 const oneCall = (lat, lon) => {
-    var newAPIURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${APIKey}`;
-
+    // Defines new URL
+    let newAPIURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${APIKey}`;
+    // Gets API data
     fetch(newAPIURL).then(function (response) {
         if(response.ok) {
             response.json().then(function(data) {
                 // Do all the stuff here
                 console.log(data);
-                // Add UV index to Daily Forcast
+                // Adds UV index to Daily Forcast
                 let UVIndex = data.current.uvi;
                 document.getElementById('UVIndexToday').innerHTML = UVIndex;
+                // Adds class to style UV index based on UV index value
                 document.getElementById('UVIndexToday').setAttribute('class',getUVIndexStyling(UVIndex));
-                // First Block
+                // loops through next 5 days and renders weather details
                 for (let i = 0; i < 5; i++) {
                     // Changes Date to relevant day
                     document.getElementById("dateDay" + (i + 1)).textContent = moment().add(i + 1, 'd').format('DD-MM-YY');
@@ -70,17 +72,18 @@ const oneCall = (lat, lon) => {
                     document.getElementById('tempDay' + (i + 1)).textContent = `Temp:\n${temp} °C`;
                     // Renders Wind on page
                     document.getElementById('windDay' + (i + 1)).textContent = 'Wind:\n' + data.daily[i].wind_speed + ' M/Hr';
-                    // Renders Humidity
+                    // Renders Humidity on page
                     document.getElementById('humidityDay' + (i + 1)).textContent = 'Humidity:\n' + data.daily[i].humidity;
                 }
             })
         }
+        // clears search input
         document.getElementById('userCityInput').value = "";
     })
+    // displays information
     document.getElementById("searchResultsContainer").style.visibility = "visible";
-
-}
-// Function to get UV styling 
+};
+// Styles UV index based conditionally 
 const getUVIndexStyling = (value) => {
     if (value <= 2) {
         return 'low';
@@ -93,27 +96,25 @@ const getUVIndexStyling = (value) => {
     } else {
         return 'extreme';
     }
-}
-
-// Local Storage Functions
+};
+// Defines local storage object
 let state = {
     previous_search: []
 };
-
+// Gets data from local storage
 function loadState() {
     let json = localStorage.getItem("Weather-searches");
 
     if (json !== null) {
         state = JSON.parse(json);
     }
-}
-
+};
+// Saves data to local storage
 function saveState() {
     let json = JSON.stringify(state);
-
     localStorage.setItem("Weather-searches", json);
-}
-
+};
+// Renders previous search list
 const renderPreviousSearchList = () => {
     document.getElementById('previousSearches').innerHTML = "";
     // Gets local storage
@@ -135,17 +136,14 @@ const renderPreviousSearchList = () => {
         // appends to the search history element
         previousSearchList.appendChild(button);
     }
-}
-
+};
+// Handles search button click
 const handleSearchButtonClick = () => {
     // Defines variable city as the value searched by the user
     let city = document.getElementById('userCityInput').value;
     // calls get city forcast
     getCityForecast(city);
-}
-        
-// add event listener to previous searches list that 
-
+};
 // Adds event listener to search button on click
 document.getElementById('searchButton').addEventListener('click', handleSearchButtonClick);
 // Adds event listener to enter button
@@ -154,10 +152,7 @@ window.addEventListener('keydown', (event) => {
         handleSearchButtonClick();
     }
 });
-
-
-
 // On Page Load
 renderPreviousSearchList();
-// Results are hidden then when data loads display results
+// Hides results section by default
 document.getElementById("searchResultsContainer").style.visibility = "hidden";
